57d6b08ef39dbbebc7ab222e1fa3b825ba98671d
Mike Jed <jedmike@gmail.com>
2020-03-04 20:33:12 -0500

    iniial push

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Friends List</Title>
+      {[0,1,2].map(row =>
+      <div>
+      {friends.map(friend =>
+      // map takes json array and indexes through it and creates friend[0]-->friend[n]
+      <FriendCard
+        name={friend.name}
+        image={friend.image}
+        occupation={friend.occupation}
+        location={friend.location}
+      />
+      )
+}
+      </div>
+      )
+}
+    </Wrapper>
+  );
+}
+
+export default App;
